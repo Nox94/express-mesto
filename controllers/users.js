179e4ea6e0399167e93d397db7ca5a082718b018
Evgenia Amelina <shkarina.evgenia@yandex.ru>
2021-05-06 10:21:00 +0300

    сделано хеширование пароля, дефолт у схемы user

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 module.exports.getAllUsers = (req, res) => {
@@ -26,8 +27,18 @@ module.exports.getOneUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash, // хеш записан в базу
+    }))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
